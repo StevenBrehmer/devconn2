63a8a3c65e1fc589b19d87faf466a67672c9ca01
Steven Brehmer <srbrehmer94@gmail.com>
2020-12-23 23:31:06 -0500

    logic to add user to db with salt and hash pw

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const {check, validationResult} = require('express-validator/check')
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
+const {check, validationResult} = require('express-validator');
+
+const User = require('../../models/User');
 
 // @route   POST api/users
 // @desc    Register User
@@ -9,14 +14,62 @@ router.post("/",[
     check('name', 'Name is required').not().isEmpty(),
     check('email','Please Include a valid Email').isEmail(),
     check('password', 'Please Enter password with 6 or more').isLength({ min: 6})
-], (req , res) => {
+], async (req , res) => {
     const errors = validationResult(req);
     if(!errors.isEmpty())
     {
+        // 400 error = invalid request
         return(res.status(400).json({errors: errors.array()}))
     }
-    console.log(req.body);
-    res.send('successful post - no errors')
+
+    const { name, email, password } = req.body;
+
+    try {
+        // See if user exists
+        // instead of finding based on email:email  - shorthand is just email
+        let user = await User.findOne({email});
+
+        if(user)
+        {
+           return res.status(400).json({errors:[{mgs: 'user already registed in db'}]});
+        }
+
+        // Get users Gravatar
+        const avatar = gravatar.url(email,{
+            s:'200',
+            r:'pg',
+            d:'mm'
+        })
+
+        user = new User({
+            name, 
+            email,
+            avatar, 
+            password
+        });
+
+        // encrypt password (bcrypt)
+        // create salt to do hashing with
+        // 10 is recomended rounds in documentation ( more you have more secure - but slowerrrr)
+        const salt = await bcrypt.genSalt(10);
+
+        // with salt, we have to hash password        
+        user.password = await bcrypt.hash(password, salt);
+
+        // anything that returns a promise use await.
+        // without this being async we would use a lot of .then
+        //save user to db
+        await user.save();
+
+        // return json web token
+        res.send("User Registered");
+
+    }catch(error){
+        console.error(error.message);
+        //500 - internal server error
+        res.status(500).send('Server Error');
+    }
+    
 });
 
 
