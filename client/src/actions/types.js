ac8856f71d0f87c3c2d610ff5de9b04612cf1110
Steven Brehmer <srbrehmer94@gmail.com>
2021-01-07 13:45:37 -0500

    Add profile reducer

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -12,3 +12,6 @@ export const LOGOUT_FAIL = 'LOGOUT_FAIL';
 
 export const USER_LOADED = 'USER_LOADED';
 export const AUTH_ERROR = 'AUTH_ERROR';
+
+export const GET_PROFILE = 'GET_PROFILE';
+export const PROFILE_ERROR = 'PROFILE_ERROR';
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,12 +1,28 @@
 //*racfp
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, {useEffect} from 'react';
+// to use hooks to get profile on load, use useEffect
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {getCurrentProfile} from '../../actions/profile';
 
-const Dashboard = props => {
+
+const Dashboard = ({getCurrentProfile, auth, profile}) => {
+    useEffect( () => {
+        getCurrentProfile();
+        
+    }, []); // dont forget the [] to ensure it only runs once instead of continously.
     return <div>Dashboard</div>;
 };
+
 Dashboard.propTypes = {
+    getCurrentProfile: PropTypes.func.isRequired, // *ptfr
+    auth: PropTypes.object.isRequired, // *ptor
+    profile: PropTypes.object.isRequired, 
+};
 
-}
+const mapStateToProps = state => ({
+    auth: state.auth,
+    profile: state.profile
+});
 
-export default Dashboard;
+export default connect(mapStateToProps, {getCurrentProfile})(Dashboard);
